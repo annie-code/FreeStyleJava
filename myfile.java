b0a243f89bc2e17092e9b62e48b39e79a88e1471
Annie <annarohanbabu@gmail.com>
2023-08-16 11:07:25 +0000

    freestyleupdate3

--- myfile.java
+++ myfile.java
@@ -7,6 +7,7 @@ public static void main(String[] args)
   System.out.println("hello world ");
     System.out.println("hello world is scm before hook");
 System.out.println("hello world is scm after hook");
+System.out.println("hello world is scm edit hook");
 }
 
 }
